aa1bdd2ef855f630777cc974c483d3beca4930ee
CharbelAbouafech <abouafechcharbel@gmail.com>
2024-08-10 12:32:54 -0700

    fixed

--- Entity/Player.java
+++ Entity/Player.java
@@ -2,38 +2,32 @@ package Entity;
 
 import My2DGame.GamePanel;
 import My2DGame.KeyHandler;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-
-public class Player extends Entity{
-
+public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
     public Player(GamePanel gp, KeyHandler keyH) {
-
         this.gp = gp;
         this.keyH = keyH;
-
         setDefaultValues();
         getPlayerImage();
-
     }
-    public void setDefaultValues() {
 
+    public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
         direction = "down";
     }
+
     public void getPlayerImage() {
 
-        try{
-
+        try {
             up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
             up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
             down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
@@ -43,12 +37,11 @@ public class Player extends Entity{
             right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
             right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
 
-
-
-        }catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public void update() {
 
         if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) { // in java the upper left corner is x:0,y:0. X increased to the right and Y increased as they go down
@@ -76,7 +69,7 @@ public class Player extends Entity{
             }
         }
     }
-    }
+
     public void draw(Graphics2D g2) {
 
         // g2.setColor(Color.white);
@@ -84,43 +77,40 @@ public class Player extends Entity{
 
         BufferedImage image = null;
 
-        switch(direction) {
+        switch (direction) {
             case "up":
-                if(spriteNum == 1) {
+                if (spriteNum == 1) {
                     image = up1;
                 }
-                if(spriteNum == 2) {
+                if (spriteNum == 2) {
                     image = up2;
                 }
                 break;
-             case "down":
-                 if(spriteNum == 1) {
-                     image = down1;
-                 }
-                 if(spriteNum == 2) {
-                     image = down2;
-                 }
-                 break;
-             case "left":
-                 if(spriteNum == 1) {
-                     image = left1;
-                 }
-                 if(spriteNum == 2) {
-                     image = left2;
-                 }
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
                 break;
             case "right":
-                if(spriteNum == 1) {
+                if (spriteNum == 1) {
                     image = right1;
                 }
-                if(spriteNum == 2) {
+                if (spriteNum == 2) {
                     image = right2;
                 }
                 break;
         }
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
-
-
-
     }
 }
