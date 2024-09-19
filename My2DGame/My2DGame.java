ba40fa503507bc488b428803428652e904cd87dd
CharbelAbouafech <abouafechcharbel@gmail.com>
2024-09-19 14:56:30 -0700

    FIXED new tiles

--- My2DGame/My2DGame.java
+++ My2DGame/My2DGame.java
@@ -21,3 +21,4 @@ public class My2DGame {
             gamePanel.startGameThread();
     }
 }
+        
--- tile/TileManager.java
+++ tile/TileManager.java
@@ -42,42 +42,39 @@ public class TileManager {
 
             setup(10, "grass00",false);
             setup(11, "grass01",false);
-            setup(12, "water01",true);
-            setup(13, "water02",true);
-            setup(14, "water03",true);
-            setup(15, "water04",true);
-            setup(16, "water05",true);
-            setup(17, "water06",true);
-            setup(18, "water07",true);
-            setup(19, "water08",true);
-            setup(20, "water09",true);
-            setup(21, "water10",true);
-            setup(22, "water11",true);
-            setup(23, "water12",true);
-            setup(24, "water13",true);
-            setup(25, "road00",false);
-            setup(26, "road01",false);
-            setup(27, "road02",false);
-            setup(28, "road03",false);
-            setup(29, "road04",false);
-            setup(30, "road05",false);
-            setup(31, "road06",false);
-            setup(32, "road07",false);
-            setup(33, "road08",false);
-            setup(34, "road09",false);
-            setup(35, "road10",false);
-            setup(36, "road11",false);
-            setup(37, "road12",false);
-            setup(38, "earth",false);
-            setup(39, "wall",true);
-            setup(40, "tree",true);
 
+            setup(12, "water00",true);
+            setup(13, "water01",true);
+            setup(14, "water02",true);
+            setup(15, "water03",true);
+            setup(16, "water04",true);
+            setup(17, "water05",true);
+            setup(18, "water06",true);
+            setup(19, "water07",true);
+            setup(20, "water08",true);
+            setup(21, "water09",true);
+            setup(22, "water10",true);
+            setup(23, "water11",true);
+            setup(24, "water12",true);
+            setup(25, "water13",true);
 
+            setup(26, "road00",false);
+            setup(27, "road01",false);
+            setup(28, "road02",false);
+            setup(29, "road03",false);
+            setup(30, "road04",false);
+            setup(31, "road05",false);
+            setup(32, "road06",false);
+            setup(33, "road07",false);
+            setup(34, "road08",false);
+            setup(35, "road09",false);
+            setup(36, "road10",false);
+            setup(37, "road11",false);
+            setup(38, "road12",false);
 
-
-
-
-
+            setup(39, "earth",false);
+            setup(40, "wall",true);
+            setup(41, "tree",true);
 
 
 
