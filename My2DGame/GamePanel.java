378b7f881ebed169afac9dcd3b788f4ea0da5d58
CharbelAbouafech <100039298+CharbelAbouafech@users.noreply.github.com>
2025-08-27 19:51:06 -0700

    NPC 1

--- My2DGame/GamePanel.java
+++ My2DGame/GamePanel.java
@@ -40,7 +40,7 @@ public class GamePanel extends JPanel implements Runnable {
         // ENTITY AND OBJECT
         public Player player = new Player(this,keyH);
         public SuperObject[] obj = new SuperObject[10];
-
+        public Entity npc[] = new Entity[10];
         // GAME STATE
         public int gameState;
         public final int playState = 1;
@@ -59,6 +59,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         public void setupGame() {
             aSetter.setObject();
+            aSetter.setNPC();
             PlayMusic(0);
             gameState = playState;
 
@@ -165,7 +166,12 @@ public class GamePanel extends JPanel implements Runnable {
                     obj[i].draw(g2, this);
                 }
             }
-
+            //NPC
+            for(int i = 0; i < npc.length; i++) {
+                if(npc[i] != null) {
+                    npc[i].draw(g2);
+                }
+            }
             // PLAYER
             player.draw(g2);
 
